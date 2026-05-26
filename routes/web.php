ea32241dc080d14440f2ad20ee7303944a051af5
abdair-coca <cocaabdair@gmail.com>
2026-05-25 22:09:15 -0400

    remove route to deploy

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,3 @@ Route::get(
     '/notas/categoria/{categoria}',
     [NotaController::class, 'categoria']
 )->name('notas.categoria');
-
-use Illuminate\Support\Facades\Artisan;
-
-Route::get('/correr-migraciones', function () {
-    try {
-        Artisan::call('migrate', ['--force' => true]);
-        
-        return "¡Migraciones ejecutadas con éxito! 😎 Ya puedes ir al inicio.";
-    } catch (\Exception $e) {
-        return "Error: " . $e->getMessage();
-    }
-});
